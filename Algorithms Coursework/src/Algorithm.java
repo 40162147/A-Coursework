e773b30a47715b3fda2616ea135805339cb87c40
NAPIER-MAIL\40162147 <sgraham975@hotmail.com>
2016-11-14 12:22:03 +0000

    nearest neighbour done

--- Algorithms Coursework/src/Algorithm.java
+++ Algorithms Coursework/src/Algorithm.java
@@ -1,5 +1,8 @@
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Algorithm 
 {
@@ -7,20 +10,27 @@ public class Algorithm
 	public static void main(String[] args) 
 	{	
 		String fName;
-		fName = "berlin52.tsp";
+		fName = "src/berlin52.tsp";
 		
 		ArrayList<Point2D> cities = TSBLib.loadTSPLib(fName);
 		ArrayList<Point2D> result = new ArrayList<Point2D>();
+		ArrayList<Point2D> higher = new ArrayList<Point2D>();
+		ArrayList<Point2D> lower = new ArrayList<Point2D>();
 		
-		Point2D currentCity = cities.remove(0);
+		Point2D currentCity = cities.get(0);
 		Point2D closest = null;
 	
-		//While (cities.size >0) Repeat until all cities have been added
+		System.out.println("Before " + TourLength.routeLength(cities));
+		System.out.println(cities.size());
+		
+		//Nearest Neighbour
+		/**While (cities.size >0) Repeat until all cities have been added
 		while(cities.size() > 0)
 		{
 			//result.add(currentCity) Add current city to the result
 			result.add(currentCity);
 			
+			cities.remove(currentCity);
 			
 			//distance = Infinity
 			double distance = Double.POSITIVE_INFINITY;
@@ -37,12 +47,96 @@ public class Algorithm
 					distance = getDistance(currentCity, possible);
 				}
 			}
-			//Remove the closest city so we don’t consider it again
-			cities.remove(closest);
+			
 			//The closest city is now the current city
 			currentCity = closest;
 			//End while
 		}
+		**/
+		
+		
+		//My algorithm
+		
+		result.add(currentCity);
+		cities.remove(currentCity);
+		
+		for(Point2D possible  : cities)
+		{
+			//getDistance() returns the distance between 2 cities
+			//if it is closer than any examined so far let’s take note
+			if (possible.getY() <= result.get(0).getY())
+			{
+				lower.add(possible);
+			}
+			else
+			{
+				higher.add(possible);
+			}
+		}
+		
+		Collections.sort(higher, new Comparator<Point2D>() 
+		{
+		    public int compare(Point2D p1, Point2D p2) 
+		    {
+		        return Double.compare(p1.getY(), p2.getY());
+		    }
+		});
+		
+		Collections.sort(lower, new Comparator<Point2D>() 
+		{
+		    public int compare(Point2D p1, Point2D p2) 
+		    {
+		        return Double.compare(p1.getY(), p2.getY());
+		    }
+		});
+		
+		
+		for (Point2D possible  : lower)
+		{
+			System.out.println(possible);
+		}
+		
+		/**while(lower.size() > 0)
+		{
+			
+			double distance = Double.POSITIVE_INFINITY;
+			
+			for(Point2D possible  : lower)
+			{
+				if (getDistance(currentCity, possible) < distance)
+				{
+					closest = possible;
+					distance = getDistance(currentCity, possible);
+				}
+			}
+			currentCity = closest;
+			result.add(currentCity);
+			lower.remove(currentCity);
+		}
+			
+		while(higher.size() > 0)
+		{
+			
+			double distance = Double.POSITIVE_INFINITY;
+			
+			for(Point2D possible  : higher)
+			{
+				if (getDistance(currentCity, possible) < distance)
+				{
+					closest = possible;
+					distance = getDistance(currentCity, possible);
+				}
+			}
+			currentCity = closest;
+			result.add(currentCity);
+			higher.remove(currentCity);
+		}
+		**/	
+			
+		
+		
+		System.out.println("After " + TourLength.routeLength(result));
+
 	}
 	
 	
